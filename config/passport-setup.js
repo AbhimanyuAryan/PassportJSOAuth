8c476abafee8b2b3117ef03cccdac0912b10c59f
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2018-08-09 16:11:31 +0530

    Mongodb find user and no duplicates

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -10,16 +10,20 @@ passport.use(
         clientSecret: keys.google.clientSecret,
         callbackURL: '/auth/google/redirect'
     }, (accessToken, refreshToken, profile, done) => {
-        // passport callback function
-        // comes here and calls the call back
-        console.log(profile);
-        console.log('passport callback function fired');
-         // run on this at it will hang now
-         new User({
-             username: profile.displayName,
-             googleId: profile.id,
-         }).save().then((newUser) => {
-             console.log('new user created' + newUser);
-         });
+        // check if user already exists in our db
+        User.findOne({googleId: profile.id}).then((currentUser) => {
+            if(currentUser){
+                // already have the user
+                console.log('user is: ', currentUser);
+            } else {
+                // if not, create user in our db
+                new User({
+                    username: profile.displayName,
+                    googleId: profile.id,
+                }).save().then((newUser) => {
+                    console.log('new user created' + newUser);
+                });
+            }
+        })
     })
 );
