329304e0a4415b0cf841711e0177c55ac5ad281e
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2018-08-09 15:58:16 +0530

    Mongodb usage

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./keys');
+const User = require('../models/user-model');
 
 passport.use(
     new GoogleStrategy({
@@ -12,6 +13,13 @@ passport.use(
         // passport callback function
         // comes here and calls the call back
         console.log(profile);
-        console.log('passport callback function fired'); // run on this at it will hang now
+        console.log('passport callback function fired');
+         // run on this at it will hang now
+         new User({
+             username: profile.displayName,
+             googleId: profile.id,
+         }).save().then((newUser) => {
+             console.log('new user created' + newUser);
+         });
     })
 );
