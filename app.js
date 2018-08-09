692963e7fe9b3413fff20cf7119ad46da7545760
Abhimanyu Aryan <abhimanyu.chat@gmail.com>
2018-08-09 10:45:00 +0530

    Fixing bug

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const authRoutes = require('./routes/auth-routes');
+const passportSetup = require('./config/passport-setup');
 
 const app = express();
 
-// setup view engine
+// set view engine
 app.set('view engine', 'ejs');
-
-// create home route
-app.get('/', (req, res) => {
-    res.render('home');
-})
 
 // set up routes
 app.use('/auth', authRoutes);
 
+// create home route
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
 app.listen(3000, () => {
     console.log('app now listening for requests on port 3000');
-})
+});
